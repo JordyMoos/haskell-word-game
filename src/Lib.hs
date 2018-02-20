84d9d984ee032c8b64b31d8a93197da6ff6b5965
Jordy Moos <jordy.moos@ing.com>
2018-02-20 18:01:46 +0100

    Start finding words in the grid

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,15 +3,40 @@ module Lib
     , languages
     , formatGrid
     , outputGrid
+    , findWord
+    , findWordInLine
+    , findWords
     ) where
 
 
-outputGrid :: [String] -> IO ()
+import Data.List (isInfixOf)
+import Data.Maybe (catMaybes)
+
+type Grid = [String]
+
+outputGrid :: Grid -> IO ()
 outputGrid grid = putStrLn (formatGrid grid)
 
 
-formatGrid :: [String] -> String
+formatGrid :: Grid -> String
 formatGrid = unlines
+
+
+findWord :: Grid -> String -> Maybe String
+findWord grid word =
+  let
+    lines = grid ++ (map reverse grid)
+    found = or $ map (findWordInLine word) lines
+  in
+    if found then Just word else Nothing
+
+
+findWords :: Grid -> [String] -> [String]
+findWords grid words = catMaybes $ map (findWord grid) words
+
+
+findWordInLine :: String -> String -> Bool
+findWordInLine = isInfixOf
 
 
 grid =
